d4010bfdcb65a5537bdf1110edd104651e1cf3a1
Lefteris Nikoltsios <lefteris.nikoltsios@gmail.com>
2015-09-30 17:38:21 +0300

    Update url definitions according to new core.

--- loosecms_link/urls.py
+++ loosecms_link/urls.py
@@ -2,8 +2,6 @@
 from django.conf.urls import url
 from .views import *
 
-app_urlpatterns = [
+urlpatterns = [
     url(r'^link/(?P<category_slug>[0-9A-Za-z-_.]+)/(?P<slug>[0-9A-Za-z-_.]+)$', link, name='link-info'),
 ]
-
-urlpatterns = []
